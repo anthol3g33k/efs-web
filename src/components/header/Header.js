5a85baf847996d08925e9aae2f1727a967694f3d
Anthony LANNOY <anthony.lannoy@intech.lu>
2018-09-10 14:28:10 +0200

    Remove logo size in html

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <nav class="navbar navbar-expand-lg navbar-light bg-white">
         <div class="container">
           <Link class="navbar-brand" to="/" rel="follow">
-            <img src="/img/logo.jpg" width="84" height="96" class="d-inline-block align-top" alt="" />
+            <img src="/img/logo.jpg" class="d-inline-block align-top logo" alt="" />
             <h1 class="blue ml-4 brand">EFS Grand Est</h1>
           </Link>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
